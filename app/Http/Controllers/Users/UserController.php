bb29159dd7f6830b8b9ef559c2dcd00c341153ad
Cristian Torres <desancheztorres@gmail.com>
2021-04-13 18:37:44 +0100

    Added user update

--- app/Http/Controllers/Users/UserController.php
+++ app/Http/Controllers/Users/UserController.php
@@ -3,11 +3,31 @@
 namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateUserRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller {
-    public function destroy() {
+
+    public function update(UpdateUserRequest $request)
+    {
+        $user = $request->user();
+
+        $user->update([
+            'name' => $request->get('name', $user->name),
+            'lastname' => $request->get('lastname', $user->lastname),
+            'email' => $request->get('email', $user->email),
+            'password' => Hash::make($request->password),
+        ]);
+
+        return $user;
+    }
+
+    public function destroy()
+    {
         request()->user()->delete();
 
         return response(null, 204);
     }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::delete('users', [UserController::class, 'destroy'])->middleware('auth:sanctum');
+Route::patch('users', [UserController::class, 'update'])->middleware('auth:sanctum');
 
 Route::post('users', CreateUserController::class);
-Route::patch('users', UpdateUserController::class)->middleware('auth:sanctum');
+//Route::patch('users', UpdateUserController::class)->middleware('auth:sanctum');
 Route::post('login', LoginController::class);
 Route::post('/sanctum/token', CreateUserToken::class);
 Route::post('logout', UserLogoutController::class);
